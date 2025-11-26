fe51046bd9da67528e3d1f0d52f6b5f5bbe410e5
monikajha100 <monikailasharma@gmail.com>
2025-11-26 18:02:17 +0530

    Delete dist directory

--- dist/middleware/upload.middleware.d.ts
+++
@@ -1,5 +0,0 @@
-import multer from 'multer';
-import { Request } from 'express';
-export declare const upload: multer.Multer;
-export declare const handleUploadError: (error: any, _req: Request, res: any, next: any) => any;
-//# sourceMappingURL=upload.middleware.d.ts.map
